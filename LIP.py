e43f0ef95d8d30ead0b0215ac0aa04b82fc5d8ca
Subhadip11290157 <sroy84766@gmail.com>
2024-10-22 23:06:11 +0530

    Removed an unintended indent causing lines to start drawing from the previous end-point instead of from the current position of the index finger, which looks like a bug.

--- LIP.py
+++ LIP.py
@@ -150,8 +150,8 @@ while running:
                 cv2.line(frame, (xp, yp), (x1, y1), color=draw_color, thickness=brush_thickness)
                 cv2.line(image_canvas, (xp, yp), (x1, y1), color=draw_color, thickness=brush_thickness)
 
-            # Update the previous point to the current point
-            xp, yp = x1, y1  # (i)
+        # Update the previous point to the current point
+        xp, yp = x1, y1  # (i)
 
     # Convert the drawing canvas to grayscale
     img_gray = cv2.cvtColor(image_canvas, cv2.COLOR_BGR2GRAY)
